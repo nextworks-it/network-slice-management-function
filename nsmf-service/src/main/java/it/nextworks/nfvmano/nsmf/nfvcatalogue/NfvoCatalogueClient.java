ebc722351d397611c3fe0d70779220957f126b5c
Nicola Venturi <n.venturi@nextworks.it>
2022-04-12 17:35:22 +0200

    5G-Catalogue integration within NSMF

--- nsmf-service/src/main/java/it/nextworks/nfvmano/nsmf/nfvcatalogue/NfvoCatalogueClient.java
+++ nsmf-service/src/main/java/it/nextworks/nfvmano/nsmf/nfvcatalogue/NfvoCatalogueClient.java
@@ -1,44 +1,114 @@
 package it.nextworks.nfvmano.nsmf.nfvcatalogue;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import it.nextworks.nfvmano.libs.descriptors.sol006.Nsd;
 import it.nextworks.nfvmano.libs.descriptors.sol006.Vnfd;
+import it.nextworks.nfvmano.libs.vs.common.utils.BaseRestClient;
+import it.nextworks.nfvmano.nsmf.nfvcatalogue.elements.NsdMapping;
+import it.nextworks.nfvmano.nsmf.nfvcatalogue.elements.VnfdMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 @Service
-public class NfvoCatalogueClient {
+public class NfvoCatalogueClient extends BaseRestClient {
+    private final static Logger log= LoggerFactory.getLogger(NfvoCatalogueClient.class);
+
+    @Value("${5g.catalogue.baseurl}")
+    private String catalogueBaseUrl;
 
     private ObjectMapper mapper;
 
     public NfvoCatalogueClient(){
-        this.mapper=new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        super();
+        this.mapper=new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        this.mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        this.mapper.enable(SerializationFeature.INDENT_OUTPUT);
     }
 
     public Nsd getNsdById(String nsdId){
-        Nsd nsd=null;
+        log.debug("Received request to retrieve NSD with ID {}", nsdId);
+        String nsdInfoId=getNsdInfoId(nsdId);
+        String url=catalogueBaseUrl+"/nsd/v1/ns_descriptors/"+nsdInfoId+"/nsd_content";
+        ResponseEntity<String> httpResponse= performHTTPRequest(null, url, HttpMethod.GET);
+        String response=manageHTTPResponse(httpResponse, "Impossible to find NSD with ID " + nsdId, "NSD with ID " + nsdId + " found", HttpStatus.OK);
         try {
-            nsd = mapper.readValue(new File("/home/nicola/network-slice-management-function/nsmf-app/src/main/resources/nsds/" + nsdId + ".json"), Nsd.class);
+            Nsd nsd;
+            if(response.startsWith("{")) {
+                 nsd= mapper.readValue(response, Nsd.class);
+            } else {
+                ObjectMapper ymalMapper= new ObjectMapper(new YAMLFactory());
+                nsd= ymalMapper.readValue(response, Nsd.class);
+            }
+            return nsd;
         } catch (IOException e){
-            e.printStackTrace();
+            log.error("No NSD with ID {} found", nsdId);
         }
-        return nsd;
+        return null;
     }
 
     public Vnfd getVnfdById(String vnfdId){
-        Vnfd vnfd=null;
-        try{
-            vnfd =mapper.readValue(new File("/home/nicola/network-slice-management-function/nsmf-app/src/main/resources/vnfds/"+vnfdId+".json"), Vnfd.class);
+        log.debug("Received request to retrieve VNFD with ID {}", vnfdId);
+        String vnfdInfoId=getVnfdInfoId(vnfdId);
+        String url=catalogueBaseUrl+"/vnfpkgm/v1/vnf_packages/"+vnfdInfoId+"/vnfd";
+        ResponseEntity<String> httpResponse= performHTTPRequest(null, url, HttpMethod.GET);
+        String response=manageHTTPResponse(httpResponse, "Impossible to find VNFD with ID " + vnfdId, "VNFD with ID " + vnfdId + " found", HttpStatus.OK);
+        try {
+            Vnfd vnfd;
+            if(response.startsWith("{"))
+                vnfd = mapper.readValue(response, Vnfd.class);
+            else {
+                ObjectMapper ymalMapper= new ObjectMapper(new YAMLFactory());
+                vnfd= ymalMapper.readValue(response, Vnfd.class);
+            }
+            return vnfd;
         } catch (IOException e){
-            e.printStackTrace();
+            log.error("No VNFD with ID {} found", vnfdId);
         }
-        return vnfd;
+        return null;
+    }
+
+    private String getNsdInfoId(String nsdId){
+        List<NsdMapping> mapping=null;
+        String url=catalogueBaseUrl+"/nsd/v1/ns_descriptors";
+        ResponseEntity<String> httpResponse=performHTTPRequest(null, url, HttpMethod.GET);
+        try{
+            mapping=mapper.readValue(manageHTTPResponse(httpResponse, "Impossible to find NSD packages", "NSD packages found", HttpStatus.OK), new TypeReference<List<NsdMapping>>(){});
+        }catch (IOException e){
+            log.error("No NSD packages found");
+        }
+        for(NsdMapping m: mapping){
+            if (nsdId.equals(m.getNsdId()))
+                return m.getId();
+        }
+        return null;
+    }
+
+    private String getVnfdInfoId(String vnfdId){
+        List<VnfdMapping> mapping=null;
+        String url=catalogueBaseUrl+"/vnfpkgm/v1/vnf_packages";
+        ResponseEntity<String> httpResponse=performHTTPRequest(null, url, HttpMethod.GET);
+        try{
+            mapping=mapper.readValue(manageHTTPResponse(httpResponse, "Impossible to find VNFD packages", "VNFD packages found", HttpStatus.OK), new TypeReference<List<VnfdMapping>>(){});
+        }catch (IOException e){
+            log.error("No VNFD packages found");
+        }
+        for(VnfdMapping m: mapping){
+            if(vnfdId.equals(m.getVnfdId()))
+                return m.getId();
+        }
+        return null;
     }
 }
--- nsmf-service/src/main/java/it/nextworks/nfvmano/nsmf/ra/algorithms/rest/client/ResourceAllocationRestClient.java
+++ nsmf-service/src/main/java/it/nextworks/nfvmano/nsmf/ra/algorithms/rest/client/ResourceAllocationRestClient.java
@@ -1,62 +1,21 @@
 package it.nextworks.nfvmano.nsmf.ra.algorithms.rest.client;
 
 import it.nextworks.nfvmano.libs.vs.common.ra.elements.ExternalAlgorithmRequest;
+import it.nextworks.nfvmano.libs.vs.common.utils.BaseRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
-public class ResourceAllocationRestClient {
+public class ResourceAllocationRestClient extends BaseRestClient {
     private static final Logger log = LoggerFactory.getLogger(ResourceAllocationRestClient.class);
 
     private String externalAlgorithmBaseUrl;
-    private RestTemplate restTemplate;
     private String cookies;
 
     public ResourceAllocationRestClient(){}
 
     public ResourceAllocationRestClient(String externalAlgorithmBaseUrl){
         this.externalAlgorithmBaseUrl=externalAlgorithmBaseUrl;
-        this.restTemplate=new RestTemplate();
-
-    }
-
-    private ResponseEntity<String> performHTTPRequest(Object request, String url, HttpMethod httpMethod) {
-        HttpHeaders header = new HttpHeaders();
-        header.add("Content-Type", "application/json");
-        if (this.cookies != null) {
-            header.add("Cookie", this.cookies);
-        }
-
-        HttpEntity<?> httpEntity = new HttpEntity<>(request, header);
-
-        try {
-            ResponseEntity<String> httpResponse =
-                    restTemplate.exchange(url, httpMethod, httpEntity, String.class);
-            HttpStatus code = httpResponse.getStatusCode();
-            return httpResponse;
-        } catch (RestClientException e) {
-            log.info("Message received: " + e.getMessage());
-            return null;
-        }
-    }
-
-    private String manageHTTPResponse(ResponseEntity<?> httpResponse, String errMsg, String okCodeMsg, HttpStatus httpStatusExpected) {
-        if (httpResponse == null) {
-            log.info(errMsg);
-            return null;
-        }
-
-        if (httpResponse.getStatusCode().equals(httpStatusExpected)) log.info(okCodeMsg);
-        else log.info(errMsg);
-
-        log.info("Response code: " + httpResponse.getStatusCode().toString());
-
-        if (httpResponse.getBody() == null) return null;
-
-        log.info(("Body response: " + httpResponse.getBody().toString()));
-        return httpResponse.getBody().toString();
     }
 
     public String computeResourceAllocation(ExternalAlgorithmRequest request){
