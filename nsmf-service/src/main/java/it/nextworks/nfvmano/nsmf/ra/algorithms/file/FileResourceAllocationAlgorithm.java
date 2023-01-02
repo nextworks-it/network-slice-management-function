98f90fec4d98a2651fd2ab58b6ea15ad15fe12ec
Mike-no <deangelismichael98@gmail.com>
2023-01-02 15:41:09 +0100

    resource allocation file as list, filtering by nsst id

--- nsmf-service/src/main/java/it/nextworks/nfvmano/nsmf/ra/algorithms/file/FileResourceAllocationAlgorithm.java
+++ nsmf-service/src/main/java/it/nextworks/nfvmano/nsmf/ra/algorithms/file/FileResourceAllocationAlgorithm.java
@@ -1,5 +1,6 @@
 package it.nextworks.nfvmano.nsmf.ra.algorithms.file;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.nextworks.nfvmano.libs.vs.common.exceptions.FailedOperationException;
 import it.nextworks.nfvmano.libs.vs.common.exceptions.MalformattedElementException;
@@ -10,18 +11,17 @@ import it.nextworks.nfvmano.libs.vs.common.ra.messages.compute.ResourceAllocatio
 import it.nextworks.nfvmano.libs.vs.common.ra.messages.compute.ResourceAllocationComputeResponse;
 import it.nextworks.nfvmano.nsmf.ra.ResourceAllocationComputeService;
 import it.nextworks.nfvmano.nsmf.ra.algorithms.BaseResourceAllocationAlgorithm;
-import it.nextworks.nfvmano.nsmf.ra.algorithms.stat.StaticAlgorithmNXW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class FileResourceAllocationAlgorithm extends BaseResourceAllocationAlgorithm {
     private static final RAAlgorithmType type =RAAlgorithmType.FILE ;
@@ -39,19 +39,28 @@ public class FileResourceAllocationAlgorithm extends BaseResourceAllocationAlgor
         log.debug("Received ResourceAllocationCompute request");
         try {
             ObjectMapper mapper = new ObjectMapper();
+            List<ResourceAllocationComputeResponse> defaultResponses;
             ResourceAllocationComputeResponse defaultResponse;
 
             if(!defaultRaFilePath.startsWith("/")) {
                 String resourcePath = defaultRaFilePath;
                 Resource resourceSpec = new ClassPathResource(resourcePath);
                 InputStream resource = resourceSpec.getInputStream();
-                defaultResponse= mapper.readValue(resource, ResourceAllocationComputeResponse.class);
+                defaultResponses = mapper.readValue(resource,
+                        new TypeReference<List<ResourceAllocationComputeResponse>>(){});
             } else {
-
                 //TODO: ADD CONTROL
                 log.debug("Find a valid path");
-                defaultResponse=mapper.readValue(new File(defaultRaFilePath), ResourceAllocationComputeResponse.class);
+                defaultResponses = mapper.readValue(new File(defaultRaFilePath),
+                        new TypeReference<List<ResourceAllocationComputeResponse>>(){});
             }
+
+            defaultResponse =
+                    defaultResponses.stream().filter(r -> request.getNst().getNsst().getNsstId()
+                            .equals(r.getNsResourceAllocation().getNssResourceAllocations().get(0).getNsstId()))
+                            .collect(Collectors.toList()).get(0);
+
+            log.debug("Selected Resource Allocation {}", mapper.writeValueAsString(defaultResponse));
 
             NsResourceAllocation nsResourceAllocation = new NsResourceAllocation(UUID.randomUUID().toString(),request.getNsiId(), defaultResponse.getNsResourceAllocation().getNssResourceAllocations() );
             ResourceAllocationComputeResponse response = new ResourceAllocationComputeResponse(request.getRequestId(), nsResourceAllocation, true);
